db494319cd4187d681591dd1d69db94c64dd77d3
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-20 17:44:30 +0200

    start with class "Prodotto"

---
+++ src/org/inheritance/java/shop/Prodotto.java
@@ -0,0 +1,11 @@
+package org.inheritance.java.shop;
+
+public class Prodotto {
+
+	private int code;
+	private String name;
+	private String marca;
+	private double price;
+	private double tax;
+
+}

40753f9cbca3e7d4b30f78e6f1789ecb472bb7e8
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-21 09:55:25 +0200

    feat class "Prodotto"

--- src/org/inheritance/java/shop/Main.java
+++ src/org/inheritance/java/shop/Main.java
@@ -3,7 +3,7 @@ package org.inheritance.java.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/org/inheritance/java/shop/Prodotto.java
+++ src/org/inheritance/java/shop/Prodotto.java
@@ -4,8 +4,74 @@ public class Prodotto {
 
 	private int code;
 	private String name;
-	private String marca;
+	private String brand;
 	private double price;
 	private double tax;
-
+	
+	//Costruttore
+	public Prodotto(int code, String name, String brand, double price, double tax) {
+		this.code = code;
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+		this.tax = tax;
+		
+	}
+	
+	//Getter Codice
+	public int getCode() {
+		return code;
+	}
+	
+	//Getter e Setter per il Nome
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//Getter e Setter per il Brand
+	public String getBrand() {
+		return brand;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	//Getter e Setter per il Prezzo
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	//Getter e Setter per il prezzo comprensivo di IVA
+	public double getTax() {
+		return tax;
+	}
+	
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	
+	//Metodo per ottenere il prezzo base
+	public double getBasePrice() {
+		return price;
+	}
+	
+	//Metodo per ottenere il prezzo comprensivo di IVA
+	public double getPriceWithTax() {
+		return price + (price * tax / 100);
+	}
+	
+	//Nome esteso
+	public String getExtendedName() {
+		return code + "-" + name;
+	}
+	
 }

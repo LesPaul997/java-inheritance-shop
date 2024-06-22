e058cae9dc2be5e1e403e020e7669bdc0efdad33
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-22 10:15:29 +0200

    add class "Televisori"

---
+++ src/org/inheritance/java/shop/Televisore.java
@@ -0,0 +1,28 @@
+package org.inheritance.java.shop;
+
+public class Televisore extends Prodotto{
+	private int dimensions;
+	private boolean isSmart;
+	
+	public Televisore(int code, String name, String brand, double price, double tax, int dimensions, boolean isSmart) {
+		super(code, name, brand, price, tax);
+		this.dimensions = dimensions;
+		this.isSmart = isSmart;
+	}
+	
+	public int getDimensions() {
+		return dimensions;
+	}
+	
+	public void setDimensions(int dimensions) {
+		this.dimensions = dimensions;
+	}
+	
+	public boolean isSmart() {
+		return isSmart;
+	}
+	
+	public void setSmart(boolean isSmart) {
+		this.isSmart = isSmart;
+	}
+}
